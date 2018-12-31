1fff0d19fb1a92d23d013802264b3bd62ed8a7ab
muskanaul <mk2aulak@edu.uwaterloo.ca>
2018-12-30 19:39:27 -0500

    fix heroku deployment issues

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 from flask import Flask, jsonify
 from crawlers.sephora import getSephoraProducts
 app = Flask(__name__)
+port = int(os.environ.get('PORT', 5000)) 
 
 @app.route('/api/sephora')
 def index():
     return jsonify(getSephoraProducts())
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=port)
