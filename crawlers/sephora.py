ed0fe7314f69e87ada3bb5bfd8c2eb6740af28eb
muskanaul <mk2aulak@edu.uwaterloo.ca>
2018-12-31 22:26:03 -0500

    fixed json response object bug
    The response object was being concatenated => fix: removed global variables

--- crawlers/sephora.py
+++ crawlers/sephora.py
@@ -12,34 +12,30 @@ from models.Product import Product
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11', 'Accept': 'application/json', 'referer': 'https://google.ca'}
 base_url ='https://www.sephora.com/api/catalog/search?country_switch=ca&type=keyword&q=sale&icid2=meganav_sale_top_nav_link%3fpageSize=-1&currentPage=1&content=true&includeRegionsMap=true&page=60&currentPage=1'
 totalProducts = 0
-categories = []
 pageSize = 60
-responseObject = {}
 
 #debug values
 _DEBUG_= 0 #set to 1 for debug mode 
 testValue = 6
 
 def getSephoraProducts():
-    getCategoryAndRecordInfo()
+    responseObject = {}
+    categories = getCategoryAndRecordInfo()
     if _DEBUG_:
         test()        
     else:
         for c in categories:
             c.products = getCategorySpecificRecords(c.node, c.name, c.recordCount)
-
     responseObject = [c.convertToJson() for c in categories]
     return responseObject
 
 def getCategoryAndRecordInfo():
     #get data 
+    categories = []
     req = Request(url=base_url, headers=headers) 
     res = urlopen(req).read().decode()
     categoryData = json.loads(res)
-    
-    #set record information to global variables from dictionary
-    responseObject["totalProducts"] = categoryData["totalProducts"]
-    totalProducts = categoryData["totalProducts"]
+
 
     #set categoriesInformation
     for x in categoryData["categories"]:
@@ -49,7 +45,8 @@ def getCategoryAndRecordInfo():
     #set node information for future category specific API calls
     if _DEBUG_:
         for c in categories:
-            print(c.name + " : " + str(c.node) + " : " + str(c.recordCount) + " : " + str(totalProducts))
+            print(c.name + " : " + str(c.node) + " : " + str(c.recordCount) + " : " + str(totalProducts))  
+    return categories
 
 def getCategorySpecificRecords(node, name, recordCount):
     numCalls = int(math.ceil(float(recordCount)/pageSize))
